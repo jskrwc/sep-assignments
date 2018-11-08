09286fede36d392b5a38cfc26e73b0de67273ecd
Jim Knopf <jsk4@mac.com>
2018-11-07 22:02:46 -0800

    assignment 01 - lines / pixels completed

--- 01-data-structures/01-introduction-to-data-structures/line/line.rb
+++ 01-data-structures/01-introduction-to-data-structures/line/line.rb
@@ -9,26 +9,38 @@ class Line
   end
 
   def join(person)
+    members << person
+    # members.push(person)
   end
 
   def leave(person)
+    members[index(person)] = nil
+    # members.delete_at(index(person))
+    # members.delete(person)
+    #members -= [person]
   end
 
   def front
+    members.first
   end
 
   def middle
+    members[members.length/2]
   end
 
   def back
+    members.last
   end
 
   def search(person)
+    members.include?(person) ? person : nil
+    # index(person)? member : nil
   end
 
   private
 
   def index(person)
+    self.members.index(person)
   end
 
 end
