6f5ab0d23aa9773043750305f6ca802250494b31
Jim Knopf <jsk4@mac.com>
2018-11-08 17:42:28 -0800

    assignment 02 - stacks and queues completed

--- 01-data-structures/02-stacks-and-queues/myqueue/myqueue.rb
+++ 01-data-structures/02-stacks-and-queues/myqueue/myqueue.rb
@@ -5,14 +5,27 @@ class MyQueue
   def initialize
     @queue = Array.new
     @head = @queue[0]
+    @tail = @queue[-1]
   end
 
   def enqueue(element)
+    @queue << element
+    @head = @queue[0]  # why need?
+    @tail = @queue.last
   end
 
   def dequeue
+        # @queue[0] = nil   #doesnt work
+        # result = @queue.delete[0]   #not work
+        # result = @queue.delete_at(0)  #not work-- break into 2 lines..
+    result = @queue[0]
+    @queue.delete_at(0)
+    @head = @queue[0]
+    @tail = @queue.last
+    return result
   end
 
   def empty?
+    @queue.length == 0
   end
 end
--- 01-data-structures/02-stacks-and-queues/mystack/mystack.rb
+++ 01-data-structures/02-stacks-and-queues/mystack/mystack.rb
@@ -7,11 +7,18 @@ class MyStack
   end
 
   def push(item)
+    @stack << item
+    self.top = item
   end
 
   def pop
+    tempItem = self.top
+    @stack.delete(top)  #delete at?
+    self.top = @stack.last
+    return tempItem
   end
 
   def empty?
+    @stack.length == 0
   end
 end
