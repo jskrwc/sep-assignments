4b9d7e74e54d2d37e859d5ad7d84efec22db3887
Jim Knopf <jsk4@mac.com>
2018-12-20 15:53:57 -0800

    hashes-1 completed

--- 01-data-structures/04-hashes-part-1/hashclass.rb
+++ 01-data-structures/04-hashes-part-1/hashclass.rb
@@ -5,28 +5,35 @@ class HashClass
   end
 
   def []=(key, value)
-    item = @items[index(key,size)]
-    if item.nil?      # if unoccupied / no collision
-      @items[index(key,size)] = HashItem.new(key,value)
-    elsif item.key == key && item.value != value           # if collision --> resize
-      resize
-      item.value = value
+    i = index(key, size) # make more readable -- using i for  index(key,size)
+    if @items[i] == nil      # if unoccupied / no collision
+      @items[i] = HashItem.new(key,value)
+    elsif @items[i].key == key # key is same, so it's a duplicate or overwrite with new value
+      @items[i] = HashItem.new(key,value)
+    else  # collision -- not nil, but a new key, so resize until nil
+      while @items[i].key != nil && @items[i].key != key do
+        resize
+        j = index(key, size)
+        break if @items[j] == nil
+      end
+      @items[j] = HashItem.new(key, value)
     end
-    # puts "size of hash: #{{size}}"
-    puts @items
+    # puts "size of hash: #{size}"
+    # puts @items
   end
 
 
   def [](key)
-    item = @items[index(key,@size)]
-    item.value   # what if nil, will return nil ??
+    i = index(key, size)
+    @items[i].value if @items[i]   # return value if not a nil
   end
 
   def resize
-    new_array = Array.new(size *2)
+    new_array = Array.new(size * 2)
     @items.each do |item|
-      if item  != nil?
-        new_array[index(item.key, @size)] = item
+      if item != nil
+        i = index(item.key, size * 2)
+        new_array[i] = item
       end
     end
     @items = new_array
@@ -36,12 +43,12 @@ class HashClass
   # We are hashing based on strings, let's use the ascii value of each string as
   # a starting point.
   def index(key, size)
-    key.sum % size
+    key.sum % size   #nb  this hashing is overly simple and has limited functionality -- e.g. anagrams will never be unique
   end
 
   # Simple method to return the number of items in the hash
   def size
-    @items.count
+    @items.length
   end
 
 end
