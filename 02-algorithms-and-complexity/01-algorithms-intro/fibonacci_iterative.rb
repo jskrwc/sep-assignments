9c02585b8ee4d2bb2641d428218521d1b7a4be80
Jim Knopf <jsk4@mac.com>
2019-02-22 16:55:28 -0800

    fix base case 0 issue

--- 02-algorithms-and-complexity/01-algorithms-intro/fibonacci_iterative.rb
+++ 02-algorithms-and-complexity/01-algorithms-intro/fibonacci_iterative.rb
@@ -1,9 +1,12 @@
 def fib(n)
 
+  return 0 if n == 0     # needed bc fib(0) = 0
+  # return 1 if n == 1
+
   fib_0 = 0
   fib_1 = 1
 
-  (0..n-1).each do
+  (0..n-2).each do       # can correct by using n-2, fixes fib(1) as well
     temp = fib_0
     fib_0 = fib_1
     fib_1 = temp + fib_1
